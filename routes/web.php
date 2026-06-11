ca55fd0422d9f8560d9cea946c649a4b34ac6521
Giselle <giselleposas@gmail.com>
2026-06-11 03:44:09 +0200

    update routes web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@ use App\Models\Ciudad;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/meteorologia', function () {
-    try {
-        $ciudades = \App\Models\Ciudad::all();
-        return response()->json($ciudades);
-    } catch (\Exception $e) {
-        return response()->json([
-            'error' => $e->getMessage()
-        ]);
-    }
+    $ciudades = Ciudad::all();
+    return view('meteorologia', compact('ciudades'));
 });
